af264fab0abd55a47d7b24a906c5d55f36084a3e
JustusFT <JustusFT@gmail.com>
2017-10-14 09:56:08 +0800

    Added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+topics = []
+10.times do
+  topics << FactoryGirl.create(:topic)
+end
+
+10.times do
+  question = FactoryGirl.create(:question, topics: topics.sample(rand(1..5)))
+  5.times do
+    question.votes << FactoryGirl.create(:vote, :question_vote, content: question)
+  end
+  question.save
+  rand(1..3).times do
+    answer = FactoryGirl.create(:answer, question: question)
+    5.times do
+      answer.votes << FactoryGirl.create(:vote, :answer_vote, content: answer)
+    end
+    answer.save
+    rand(0..2).times do
+      comment = FactoryGirl.create(:comment, answer: answer)
+      5.times do
+        comment.votes << FactoryGirl.create(:vote, :comment_vote, content: comment)
+      end
+      comment.save
+      rand(0..2).times do
+        FactoryGirl.create(:comment, answer: answer, parent: Comment.last)
+      end
+    end
+  end
+end
