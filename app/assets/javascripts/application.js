ade590ec9579254828c7f58333889f79922a6945
JustusFT <JustusFT@gmail.com>
2017-10-16 16:27:43 +0800

    Modified comment and answer forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,57 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  $('.comment-textarea').each(function () {
+    this.setAttribute('style', 'min-height:34px;height:34px;overflow-y:hidden;');
+  }).on('input', function () {
+    this.style.height = '0px';
+    this.style.height = (this.scrollHeight + 2) + 'px';
+  });
+
+  $('.show-new-comment-form').click(function() {
+    let form = $(this).siblings('.new-comment-form').first();
+    form.show();
+    form.find('.new-comment-textarea').first().focus();
+    $(this).siblings('.btn').andSelf().hide();
+  });
+
+  $('.show-edit-comment-form').click(function() {
+    $(this).siblings('.comment-text').first().hide();
+    $(this).siblings('.edit-comment-form').first().show();
+    $('textarea').trigger('input');
+    $(this).siblings('.btn').andSelf().hide();
+  });
+
+  $('.hide-edit-comment-form').click(function() {
+    let form = $(this).parent();
+    form.hide();
+    form.siblings('.comment-text').show();
+    form.siblings('.btn').show();
+    form[0].reset();
+  });
+
+  $('.show-edit-answer-form').click(function() {
+    $(this).siblings('.answer-text').first().hide();
+    $(this).siblings('.edit-answer-form').first().show();
+    $('textarea').trigger('input');
+    $(this).siblings('.btn').andSelf().hide();
+  });
+
+  $('.hide-edit-answer-form').click(function() {
+    let form = $(this).parent();
+    form.hide();
+    form.siblings('.answer-text').show();
+    form.siblings('.btn').show();
+    form[0].reset();
+  });
+
+  $('.new-comment-textarea').blur(function() {
+    if($(this).val() == '') {
+      let form = $(this).parent().parent();
+      form.hide();
+      form.siblings('.btn').show();
+    }
+  });
+});
