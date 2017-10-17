0612017cdcb05c3d9a00b0ab5d514142d2822eab
JustusFT <JustusFT@gmail.com>
2017-10-17 12:38:28 +0800

    Added filter for all followed topics

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,7 +8,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_and_belongs_to_many :topics
 
-  scope :topic, ->(topic_id) { joins(:topics).where("topics": { id: topic_id }) }
+  scope :topic, ->(topic_id) { joins(:topics).where("topics": { id: topic_id }).group(:id) }
 
   def get_question
     self
