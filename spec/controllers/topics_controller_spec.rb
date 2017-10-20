4378b05cdaf6931c09cfe21f6705a87a26ea98db
JustusFT <JustusFT@gmail.com>
2017-10-20 13:17:31 +0800

    Added tests for #follow and #unfollow of topics_controller

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe TopicsController, type: :controller do
+  describe "#follow" do
+    it "can create a TopicUser" do
+      topic = create(:topic)
+      user = create(:user)
+      sign_in user
+      expect {
+        post :follow, params: { topic_id: topic.id }
+      }.to change(TopicUser, :count).by 1
+    end
+  end
 
+  describe "#unfollow" do
+    it "can remove a TopicUser" do
+      topic = create(:topic)
+      user = create(:user)
+      sign_in user
+      post :follow, params: { topic_id: topic.id }
+      expect {
+        delete :unfollow, params: { topic_id: topic.id }
+      }.to change(TopicUser, :count).by -1
+    end
+  end
 end
